55f53640210a75fc81bbd4e2dfa4baf2192499e0
Tvup Torben Hansen <tvupper@gmail.com>
2016-11-08 13:10:59 +0100

    PSR Compliance

--- LeafTurnOnCC.php
+++ LeafTurnOnCC.php
@@ -2,83 +2,73 @@
 
 class LeafTurnOnCC
 {
-  private $userId = '';
-  private $password = '';
-  private $initialAppStrings = '';
-  private $lg ='en-US';
-  private $regionCode ='NE';
-  private $dcmid ='';
-  private $VIN;
-  
-  function __construct($userId, $password, $initialAppStrings, $dcmid, $VIN)
+    private $userId = '';
+    private $password = '';
+    private $initialAppStrings = '';
+    private $lg = 'en-US';
+    private $regionCode = 'NE';
+    private $dcmid = '';
+    private $VIN;
+
+    function __construct($userId, $password, $initialAppStrings, $dcmId, $VIN)
     {
-      $this->userId=$userId;
-      $this->password=$password;
-      $this->initialAppStrings=$initialAppStrings;
-      $this->dcmid=$dcmid;
-      $this->VIN=$VIN;
+        $this->userId = $userId;
+        $this->password = $password;
+        $this->initialAppStrings = $initialAppStrings;
+        $this->dcmid = $dcmId;
+        $this->VIN = $VIN;
     }
-  
-  
-  public function fire()
-  {
 
+    public function fire()
+    {
+        $postBody = 'UserId=' . urlencode($this->userId) . '&cartype=&custom_sessionid=&initial_app_strings=' . $this->initialAppStrings . '&tz=&lg=' . $this->lg . '&DCMID=&VIN=&RegionCode=' . $this->regionCode . '&Password=' . urlencode($this->password);
 
-$postFields = array('UserId'=>$this->userId, 'cartype'=>'', 'custom_sessionid'=>'', 'initial_app_strings'=>$this->initialAppStrings
-, 'tz'=>'', 'lg'=>$this->lg, 'DCMID'=>'', 'VIN'=>'', 'RegionCode'=>$this->regionCode, 'Password'=>$this->password);
+        $headerArray = array('Content-Type: application/x-www-form-urlencoded',
+            'Charset: UTF-8',
+            'User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.1; Custom Phone - 5.1.0 - API 22 - 768x1280 Build/LMY47D)',
+            'Connection: Keep-Alive',
+            'Accept-Encoding: gzip');
 
+        $curlOptions = array(
+            CURLOPT_HTTPHEADER => $headerArray,
+            CURLOPT_POSTFIELDS => $postBody,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_FOLLOWLOCATION => TRUE,
+        );
 
-$postbody = 'UserId=' . urlencode($this->userId)  . '&cartype=&custom_sessionid=&initial_app_strings=' . $this->initialAppStrings . '&tz=&lg='. $this->lg .'&DCMID=&VIN=&RegionCode=' . $this->regionCode . '&Password=' . urlencode($this->password);
+        $url = "https://gdcportalgw.its-mo.com/gworchest_0405EC/gdc/UserLoginRequest.php";
+        $handle = curl_init($url);
+        curl_setopt_array($handle, $curlOptions);
+        $content = curl_exec($handle);
 
-$headerArray = array('Content-Type: application/x-www-form-urlencoded',
-'Charset: UTF-8',
-'User-Agent: Dalvik/2.1.0 (Linux; U; Android 5.1; Custom Phone - 5.1.0 - API 22 - 768x1280 Build/LMY47D)',
-'Connection: Keep-Alive',
-'Accept-Encoding: gzip');
+        $response = json_decode($content);
+        $custom_sessionid = $response->VehicleInfoList->vehicleInfo[0]->custom_sessionid;
 
+        //New request
+        $postBody = 'UserId=' . urlencode($this->userId) . '&cartype=&custom_sessionid=' . urlencode($custom_sessionid) . '&tz=Europe%2FCopenhagen&lg=da-DK&DCMID=' . $this->dcmid . '&VIN=' . $this->VIN . '&RegionCode=' . $this->regionCode;
 
-$curlOptions = array(
-    CURLOPT_HTTPHEADER => $headerArray,
-    CURLOPT_POSTFIELDS => $postbody,
-    CURLOPT_RETURNTRANSFER => TRUE,
-    CURLOPT_FOLLOWLOCATION => TRUE,
-);
+        $curlOptions = array(
+            CURLOPT_HTTPHEADER => $headerArray,
+            CURLOPT_POSTFIELDS => $postBody,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_FOLLOWLOCATION => TRUE,
+        );
 
-$url = "https://gdcportalgw.its-mo.com/gworchest_0405EC/gdc/UserLoginRequest.php";
-$handle = curl_init($url);
-curl_setopt_array($handle, $curlOptions);
-$content = curl_exec($handle);
+        $url = "https://gdcportalgw.its-mo.com/gworchest_0405EC/gdc/ACRemoteRequest.php";
+        $handle = curl_init($url);
+        curl_setopt_array($handle, $curlOptions);
+        $content = curl_exec($handle);
 
-$response = json_decode($content);
-$custom_sessionid = $response->VehicleInfoList->vehicleInfo[0]->custom_sessionid;
+        $response = json_decode($content);
 
-//New request
-$postbody = 'UserId=' . urlencode($this->userId) .'&cartype=&custom_sessionid=' . urlencode($custom_sessionid) . '&tz=Europe%2FCopenhagen&lg=da-DK&DCMID=' . $this->dcmid . '&VIN=' . $this->VIN . '&RegionCode=' . $this->regionCode;
+        var_export($response);
 
-$curlOptions = array(
-    CURLOPT_HTTPHEADER => $headerArray,
-    CURLOPT_POSTFIELDS => $postbody,
-    CURLOPT_RETURNTRANSFER => TRUE,
-    CURLOPT_FOLLOWLOCATION => TRUE,
-);
-
-$url = "https://gdcportalgw.its-mo.com/gworchest_0405EC/gdc/ACRemoteRequest.php";
-$handle = curl_init($url);
-curl_setopt_array($handle, $curlOptions);
-$content = curl_exec($handle);
-
-$response = json_decode($content);
-
-var_export($response);
-
-curl_close($handle);
-
-var_dump($response);
-}
+        curl_close($handle);
+    }
 
 }
-$obj = new LeafTurnOnCC($argv[1],$argv[2],$argv[3],$argv[4],$argv[5]);
+
+$obj = new LeafTurnOnCC($argv[1], $argv[2], $argv[3], $argv[4], $argv[5]);
 $obj->fire();
 
-?>
 
